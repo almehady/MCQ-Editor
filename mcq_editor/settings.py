3d7597d002d4252100ea025c3ff87543b72cfd51
almehady <almehady@gmail.com>
2021-05-19 09:04:55 +0300

    updated settings with psql database

--- mcq_editor/settings.py
+++ mcq_editor/settings.py
@@ -79,10 +79,18 @@ WSGI_APPLICATION = 'mcq_editor.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'mcq_editor',
+            'USER': 'mcq_editor_user',
+            'PASSWORD': 'dhAk10eBo0k',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
+        }
 }
 
 
