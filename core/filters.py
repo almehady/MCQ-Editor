8150610dc931e85152a10c8109f029b75b0f8ac1
almehady <almehady@gmail.com>
2021-06-01 21:40:29 +0300

    multiple subject filter

--- core/filters.py
+++ core/filters.py
@@ -1,9 +1,20 @@
 import django_filters
+from django import forms
 
 from .models import QuestionBank
 
 class QuestionBankSearchFilter(django_filters.FilterSet):
     category = django_filters.AllValuesMultipleFilter(field_name='category__title')
+    SUBJECT_CHOICE = [
+        ('❖বাংলা ভাষা ও সাহিত্য❖', '❖বাংলা ভাষা ও সাহিত্য❖'),
+        ('❖English Language and Literature❖', '❖English Language and Literature❖'),
+        ('❖বাংলাদেশ ও আন্তর্জাতিক বিষয়াবলি❖', '❖বাংলাদেশ ও আন্তর্জাতিক বিষয়াবলি❖'),
+        ('❖গাণিতিক যুক্তি❖', '❖গাণিতিক যুক্তি❖'),
+        ('❖সাধারণ বিজ্ঞান❖', '❖সাধারণ বিজ্ঞান❖'),
+    ]
+    # subject = forms.CharField(label='Select Subject', widget=forms.Select(choices=SUBJECT_CHOICE))
+    subject = django_filters.AllValuesMultipleFilter(field_name='subject')
+
     class Meta:
         model = QuestionBank
         fields = '__all__'
